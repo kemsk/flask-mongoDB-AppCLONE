54ba5211f161513065ef5fd136c203f4824ee976
Kimberly Flores <20220024573@my.xu.edu.ph>
2024-11-30 10:43:01 +0800

    TRIAL

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-# importing from flask module the Flask class, the render_template function, the request function, url_for 
-# and redirect function to redirect to index home page after updating the app database
-from flask import Flask, render_template, request, url_for, redirect 
-# Mongoclient is used to create a mongodb client, so we can connect on the localhost 
-# with the default port
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
-# ObjectId function is used to convert the id string to an objectid that MongoDB can understand
-from bson.objectid import ObjectId
-# Instantiate the Flask class by creating a flask application
+
 app = Flask(__name__)
-# Create the mongodb client
-client = MongoClient('localhost', 27017)
-# Get and Post Route
-@app.route("/", methods=('GET', 'POST'))
-def index():
-    if request.method == "POST":   # if the request method is post, then insert the todo document in todos collection
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('index')) # redirect the user to home page
-    all_todos = todos.find()    # display all todo documents
-    return render_template('index.html', todos = all_todos) # render home page template with all todos
-#Delete Route
-@app.post("/<id>/delete/")
-def delete(id): #delete function by targeting a todo document by its own id
-    todos.delete_one({"_id":ObjectId(id)}) #deleting the selected todo document by its converted id
-    return redirect(url_for('index')) # again, redirecting you to the home page 
-db = client.flask_database # creating your flask database using your mongo client 
-todos = db.todos # creating a collection called "todos"
-# The dunder if __name__ code block
-if __name__ == "__main__":
-    app.run(debug=True) #running your server on development mode, setting debug to True
+
+# MongoDB connection string and database setup
+client = MongoClient('mongodb+srv://20220024573:T7CmWQ47ed9s8kpv@recipecluster.81ir5.mongodb.net/')
+db = client['RecAPI']  # Database name
+
+# Define the collections
+recipes_collection = db['Recipes']
+ingredients_collection = db['Ingredients']
+authors_collection = db['Authors']
+dietary_benefits_collection = db['Dietarybenefits']
+nutrition_info_collection = db['Nutritioninfo']
+pictures_collection = db['Pictures']
+users_collection = db['Users']
+videos_collection = db['Videos']
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Renders the HTML front end
+
+# API endpoint to get all data from Recipes collection
+@app.route('/api/recipes', methods=['GET'])
+def get_all_recipes():
+    try:
+        # Fetch all recipes from the Recipes collection
+        recipes = recipes_collection.find({})
+        response = []
+
+        # Loop through each recipe to fetch its ingredients
+        for recipe in recipes:
+            recipe_ingredients = []
+            # Fetch the ingredients that match the current recipeID from the Ingredients collection
+            ingredients = ingredients_collection.find({'recipeID': recipe['recipeID']})
+            for ingredient in ingredients:
+                recipe_ingredients.append({
+                    'name': ingredient.get('name', 'N/A'),
+                    'quantity': ingredient.get('quantity', 'N/A')
+                })
+
+            # Add the recipe along with its ingredients
+            response.append({
+                'recipeID': str(recipe['recipeID']),
+                'name': recipe.get('name', 'N/A'),
+                'instructions': recipe.get('instructions', 'N/A'),
+                'category': recipe.get('type', 'N/A'),
+                'ingredients': recipe_ingredients
+            })
+
+        # Return the recipes and ingredients in JSON format
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500  # Return an error response if something goes wrong
+
+if __name__ == '__main__':
+    app.run(debug=True)
